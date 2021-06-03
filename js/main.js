7278a5cbcae9d14bafadde3fb9e23c2ac7259c79
Dương Nguyễn <aliosa113@gmail.com>
2021-06-03 09:14:32 +0700

    all project first commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+(function($) {
+
+    jQuery(window).on('load', function() {
+
+        // WOW JS
+        new WOW().init();
+
+        // Preloader
+        var preLoder = $("#preloader");
+        preLoder.fadeOut(0);
+
+    });
+
+})(jQuery);
