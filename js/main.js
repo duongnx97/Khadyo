49b16e280bf6121442f4a85df3f7bfb0f566cefb
Duong Xuan Nguyen <duongnx97@gmail.com>
2021-06-05 11:30:55 +0700

    use meanmenu js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,16 @@
 (function($) {
 
+    "use strict"
+    jQuery(document).ready(function() {
+
+        // mobile-menu
+        $('#mobile-menu').meanmenu({
+            meanMenuContainer: '.mobile-menu',
+            meanScreenWidth: "991.99"
+        });
+
+    });
+
     jQuery(window).on('load', function() {
 
         // WOW JS
