1f326a1da9955e09b8aea84cb1752ffb9df4b293
Duong Xuan Nguyen <duongnx97@gmail.com>
2021-06-10 11:29:02 +0700

    home update - testimonial area and slider gallery area

--- js/main.js
+++ js/main.js
@@ -59,6 +59,75 @@
             });
         });
 
+        // Testimonial 
+        $('.testimonial-active').slick({
+            dots: true,
+            infinite: false,
+            speed: 1000,
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            arrows: false,
+            responsive: [{
+                    breakpoint: 992,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 1,
+                        infinite: true,
+                        dots: true
+                    }
+                },
+                {
+                    breakpoint: 768,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1
+                    }
+                },
+                {
+                    breakpoint: 480,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1
+                    }
+                }
+            ]
+        });
+
+        //   slider-gallery
+        $('.slider-gallery-active').slick({
+            dots: false,
+            arrows: false,
+            infinite: true,
+            speed: 1000,
+            autoplay: true,
+            autoplaySpeed: 2000,
+            slidesToShow: 4,
+            centerMode: true,
+            variableWidth: true,
+            responsive: [{
+                    breakpoint: 1024,
+                    settings: {
+                        slidesToShow: 3,
+
+                    }
+                },
+                {
+                    breakpoint: 600,
+                    settings: {
+                        slidesToShow: 2,
+
+                    }
+                },
+                {
+                    breakpoint: 480,
+                    settings: {
+                        slidesToShow: 2,
+
+                    }
+                }
+            ]
+        });
+
         // Countdown 
         $('.countdown').downCount({
             date: '06/28/2021 12:00:00',
